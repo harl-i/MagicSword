01508d8c88933e15c4a567ab25cdbc684d7aced5
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2024-02-27 03:02:14 +0300

    setting up the shield

--- Assets/Scripts/Sword/Shield.cs
+++ Assets/Scripts/Sword/Shield.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Shield : MonoBehaviour 
 {
     [SerializeField] private Player _player;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        transform.position = _player.transform.position;
+        transform.rotation = _player.transform.rotation;
+
+        _spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        _spriteRenderer.enabled = false;
+    }
 
     private void Update()
     {
