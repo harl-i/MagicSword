0a697913df1a45b836480d3bd5a0d6d199e69b41
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2024-06-25 02:49:42 +0300

    Fix bird

--- Assets/Scripts/StateMachine/Transitions/AttackOnWillagerTransition.cs
+++ Assets/Scripts/StateMachine/Transitions/AttackOnWillagerTransition.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class AttackOnWillagerTransition : Transition
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Willager willager))
         {
