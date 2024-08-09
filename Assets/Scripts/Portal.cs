b224330d0862391eb7cbc9767abbf087926f1f16
DESKTOP-QH0U1BD\Kharlamov <ilya.kharlamov.92@mail.ru>
2024-08-09 05:45:23 +0300

    added the logic for activating portals

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,5 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour{}
+[RequireComponent(typeof(Animator))]
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private int _soulsAmountForActivation;
+    [SerializeField] private PortalActivator _portalActivator;
+
+    private Animator _animator;
+    private int _currentSoulsAmountForActivation;
+
+    public Action SoulsCollected;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _portalActivator.PortalActivatorActivated += OnPortalActivatorActivated;
+    }
+
+    private void OnDisable()
+    {
+        _portalActivator.PortalActivatorActivated -= OnPortalActivatorActivated;
+    }
+
+    private void OnPortalActivatorActivated()
+    {
+        _animator.SetTrigger("Activation"); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Soul soul))
+        {
+            _currentSoulsAmountForActivation++;
+
+            if (_currentSoulsAmountForActivation == _soulsAmountForActivation)
+            {
+                SoulsCollected?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PortalActivator.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PortalActivator : MonoBehaviour
+{
+    [SerializeField] private Portal _portal;
+
+    private Animator _animator;
+    private bool _isActive;
+
+    public Action PortalActivatorActivated;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _isActive = false;
+    }
+
+    private void OnEnable()
+    {
+        _portal.SoulsCollected += OnSoulsCollected;
+    }
+
+    private void OnDisable()
+    {
+        _portal.SoulsCollected -= OnSoulsCollected;
+    }
+
+    private void OnSoulsCollected()
+    {
+        _animator.SetTrigger("Activation");
+        _isActive = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player) && _isActive)
+        {
+            PortalActivatorActivated?.Invoke();
+        }
+    }
+}
