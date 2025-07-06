b7bfb3585b3d7c923bac761206f6ba102dd9dd4a
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2025-07-06 22:27:00 +0300

    switches the buttons of the new game when resetting

--- Assets/Scripts/ReseterNewGamePlus.cs
+++ Assets/Scripts/ReseterNewGamePlus.cs
@@ -3,8 +3,13 @@ using YG;
 
 public class ReseterNewGamePlus : MonoBehaviour
 {
+    [SerializeField] private GameObject _newGame;
+    [SerializeField] private GameObject _newGamePlus;
+
     public void ResetTrigger()
     {
         YG2.saves.newGamePlus = 0;
+        _newGame.SetActive(true);
+        _newGamePlus.SetActive(false);
     }
 }
